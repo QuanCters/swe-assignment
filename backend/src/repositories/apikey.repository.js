2da329fd139a1f71e2b9c7a0b73a6701311dc1e0
QuanCters <quan.nguyen2212804@hcmut.edu.vn>
2024-11-26 16:05:18 +0700

    #12 - change route for fetch api key

--- backend/src/repositories/apikey.repository.js
+++ backend/src/repositories/apikey.repository.js
@@ -1,7 +1,9 @@
 "use strict";
 
 const getApiKey = async (key) => {
-  const response = await fetch(`http://localhost:3000/apikeys?key=${key}`);
+  const response = await fetch(
+    `https://json-server-s4l1.onrender.com/apikeys?key=${key}`
+  );
   return response.json();
 };
 
