24904243a47cccd4e9c642323cade2141d0c8e10
QuanCters <quan.nguyen2212804@hcmut.edu.vn>
2024-11-30 12:28:31 +0700

    #19 - add API document for student route

--- backend/src/routes/index.js
+++ backend/src/routes/index.js
@@ -8,6 +8,5 @@ router.use("/v1/api/student", require("./student.route"));
 router.use("/v1/api/user", require("./auth.route"));
 router.use("/v1/api/document", require("./document.route"));
 router.use("/v1/api/history", require("./history.route"));
-router.use("/v1/api", require("./access"));
 
 module.exports = router;
