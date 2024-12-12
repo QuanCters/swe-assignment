8368f5030410d7d1b37447cb908ca3a917120c4a
QuanCters <quan.nguyen2212804@hcmut.edu.vn>
2024-12-12 09:42:14 +0700

    remove resole in vite config

--- frontend/vite.config.ts
+++ frontend/vite.config.ts
@@ -1,7 +1,6 @@
 import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react";
 import { TanStackRouterVite } from "@tanstack/router-plugin/vite";
-import path from "path";
 
 // https://vitejs.dev/config/
 export default defineConfig(({ command }) => {
@@ -11,9 +10,6 @@ export default defineConfig(({ command }) => {
     server: {
       host: "0.0.0.0",
       port: 3000,
-    },
-    resolve: {
-      alias: [{ find: "@", replacement: path.resolve(__dirname, "") }],
     },
     build: {
       outDir: "build",
