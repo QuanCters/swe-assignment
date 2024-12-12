1a89e3ea95964d284897f8878516ac5bc16a7731
QuanCters <quan.nguyen2212804@hcmut.edu.vn>
2024-12-12 09:44:26 +0700

    change resolve path in vite config

--- frontend/vite.config.ts
+++ frontend/vite.config.ts
@@ -1,6 +1,7 @@
 import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react";
 import { TanStackRouterVite } from "@tanstack/router-plugin/vite";
+import path from "path";
 
 // https://vitejs.dev/config/
 export default defineConfig(({ command }) => {
@@ -10,6 +11,11 @@ export default defineConfig(({ command }) => {
     server: {
       host: "0.0.0.0",
       port: 3000,
+    },
+    resolve: {
+      alias: {
+        "@": path.resolve(__dirname, "./"),
+      },
     },
     build: {
       outDir: "build",
